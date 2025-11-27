47983d1db4b6f5dd12ccb608dba2c69a2645c6a6
Joshua Fomubod <joshuafomum@gmail.com>
2025-11-27 09:42:13 +0400

    fix: remove updater plugin to fix app launch crash
    The updater plugin was registered but required a config that was removed.
    This caused the app to panic on startup with:
    "Error deserializing plugins.updater within your Tauri configuration"
    
    Removed:
    - tauri-plugin-updater from Cargo.toml
    - updater plugin initialization from lib.rs
    - updater:default permission from capabilities

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1354,7 +1354,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .setup(|app| {
             let db = tauri::async_runtime::block_on(async {
